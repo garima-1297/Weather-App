af751b282dce4da621985ce8befa7217583db7f1
garima-1297 <82163615+garima-1297@users.noreply.github.com>
2022-05-18 00:06:14 +0530

    Delete .history directory

--- .history/script_20220516100220.js
+++
@@ -1,8 +0,0 @@
-let weather = {
-    "apiKey" : "a9790c01eb9c8638e8b7135aa586f1af",
-    fetchWeather: function() {
-        fetch("https://api.openweathermap.org/data/2.5/weather?q=Lucknow&units=metric&appid=a9790c01eb9c8638e8b7135aa586f1af")
-        .then((response) => response.json())
-        .then((data) => console.log(data));
-    }
-}
